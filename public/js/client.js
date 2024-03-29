87ff66a04be2de1e829b308f827148b098edf8a4
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-03-29 19:39:52 -0400

    create client feature

---
+++ public/js/client.js
@@ -0,0 +1,34 @@
+async function createClientHandler(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name-signup').value.trim()
+    const phoneNumber = document.getElementById('phoneNumber-signup').value.trim()
+    const repairOrderNumber = document.getElementById('repairOrderNumber-signup').value.trim()
+    const carModel = document.getElementById('carModel-signup').value.trim()
+    const serviceRequested = document.getElementById('serviceRequested-signup').value.trim()
+    const waitingForService = document.getElementById('waitingForService-signup').checked
+
+    if(name && phoneNumber && repairOrderNumber && carModel && serviceRequested && waitingForService) {
+        const response = await fetch('/api/clients', {
+            method: 'post',
+            body: JSON.stringify({
+                name,
+                phoneNumber,
+                repairOrderNumber,
+                carModel,
+                serviceRequested,
+                waitingForService
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText)
+        }
+    }
+
+    
+}
+
+document.querySelector('#clientForm').addEventListener('submit', createClientHandler)
